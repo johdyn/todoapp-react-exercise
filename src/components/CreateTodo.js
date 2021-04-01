cd09e79bd6134d1576f002cfc4efaf2c19041747
Johan Dynnesen <johandynnesen@gmail.com>
2021-04-01 16:30:18 +0200

    Initial commit

---
+++ src/components/CreateTodo.js
@@ -0,0 +1,20 @@
+function CreateTodo({ task, onDoneClick, onDeleteClick }) {
+  function handleDeleteClick(event) {
+    console.log(event);
+    onDeleteClick(task);
+  }
+
+  return (
+    <section className="todo-section">
+      <h2 className="heading-class">{task}</h2>
+      <button onClick={onDoneClick} className="done-button">
+        Done
+      </button>
+      <button onClick={handleDeleteClick} className="delete-button">
+        Delete
+      </button>
+    </section>
+  );
+}
+
+export default CreateTodo;
